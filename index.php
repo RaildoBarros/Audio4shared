a485f6d87d0af74a4c878ebe3b090f7031076cae
LG <LG@192.168.1.2>
2015-08-02 02:17:29 -0400

    Adição da tag ID3

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include './conexao.php'; ?>
+<?php
+include './conexao.php';
+include './id3.class.php';
+?>
 <!DOCTYPE html>
 
 <html>
@@ -17,24 +20,41 @@
 
         //Pega o conteúdo do diretório atual
         $contents = ftp_nlist($conn_id, ".");
+        ?>
 
-        ?>
+
+        <!-- LOOP -->
         <?php
-        foreach ($contents as $nomedoarquivo) {   
-            echo $nomedoarquivo;
-        ?>
-        <br>
-        <audio 
-            src="ftp://raildo.barros@gmail.com:29069218raildo@ftp.4shared.com/Pregações/<?php echo $nomedoarquivo; ?>" 
-            controls="controls" loop preload="preload" 
-            title="Quermesse - O Teatro Mágico"> 
-            <a href="<?php echo $contents[0] ?>">
-                <?php echo $contents[0] ?>
-            </a> 
-        </audio>
-        <br>
-        <?php
+        foreach ($contents as $nomedoarquivo) {
+            echo "O diretório atual agora é: " . ftp_pwd($conn_id) . "\n";
+            
+            $arquivo = $nomedoarquivo;
+            require('error.inc.php');
+            $myId3 = new ID3($arquivo);
+            if ($myId3->getInfo()) {
+                echo $myId3->getTrack() . '<br>';
+                echo $myId3->getTitle() . '<br>';
+                echo $myId3->getArtist() . '<br>';
+                echo $myId3->getAlbum() . '<br>';
+                echo $myId3->getYear() . '<br>';
+            } else {
+                echo($errors[$myId3->last_error_num]);
+            }
+            ?>
+            <br>
+            <!-- PLAYER -->
+            <audio 
+                src="<?php echo $arquivo; ?>" 
+                controls="controls" loop preload="preload" 
+                title="Quermesse - O Teatro Mágico"> 
+                <a href="<?php echo $contents[0] ?>">
+                    <?php echo $contents[0] ?>
+                </a> 
+            </audio>
+            <br>
+            <?php
         }
         ?>
+
     </body>
 </html>
